cc7672454a82061d7fbe5dd2d03f640e1a2df10d
YoannPitance <pittyo88@gmail.com>
2023-06-08 14:35:17 +0200

    14- carte 3d ok plus effet lumineux, reste faceB + retournement

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,44 +1,48 @@
 import React, { useEffect, useRef } from "react";
 
 export default function Intro() {
-  const cardContentRef = useRef(null);
+  const cardRef = useRef(null);
 
   useEffect(() => {
+    const card = cardRef.current;
+
     const handleMouseMove = (e) => {
       const { clientX, clientY } = e;
-      const { x, y, width, height } =
-        cardContentRef.current.getBoundingClientRect();
+      const { left, top, width, height } = card.getBoundingClientRect();
+      const x = clientX - left;
+      const y = clientY - top;
+      const angleY = -(x - width / 2) / 8;
+      const angleX = (y - height / 2) / 8;
+      const glareX = (x / width) * 100;
+      const glareY = (y / height) * 100;
 
-      const midCardWidth = width / 2;
-      const midCardHeight = height / 2;
-
-      const angleY = -(clientX - x - midCardWidth) / 8;
-      const angleX = (clientY - y - midCardHeight) / 8;
-
-      cardContentRef.current.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+      card.style.transform = `perspective(1000px) rotateX(${angleX}deg) rotateY(${angleY}deg) scale(1.1)`;
+      card.querySelector(
+        ".glare"
+      ).style.background = `radial-gradient(circle at ${glareX}% ${glareY}%, rgb(184, 196, 211), transparent)`;
     };
 
     const handleMouseLeave = () => {
-      cardContentRef.current.style.transform = "rotateX(0) rotateY(0) scale(1)";
+      card.style.transform =
+        "perspective(1000px) rotateX(0) rotateY(0) scale(1)";
     };
 
-    const cardContent = cardContentRef.current;
-    cardContent.addEventListener("mousemove", handleMouseMove);
-    cardContent.addEventListener("mouseleave", handleMouseLeave);
+    card.addEventListener("mousemove", handleMouseMove);
+    card.addEventListener("mouseleave", handleMouseLeave);
 
     return () => {
-      cardContent.removeEventListener("mousemove", handleMouseMove);
-      cardContent.removeEventListener("mouseleave", handleMouseLeave);
+      card.removeEventListener("mousemove", handleMouseMove);
+      card.removeEventListener("mouseleave", handleMouseLeave);
     };
   }, []);
 
   return (
     <div className="cardContainer">
-      <div className="card">
-        <div className="cardContent" ref={cardContentRef}>
+      <div className="card" ref={cardRef}>
+        <div className="cardContent">
           <img src="./media/moi2.jpg" alt="ma tête de bg!" />
-          <div className="glare"></div>
         </div>
+        <div className="glare"></div>
       </div>
     </div>
   );
