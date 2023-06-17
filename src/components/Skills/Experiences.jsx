c5ba7e00768aca1cee92421180ee8540ea373f68
YoannPitance <pittyo88@gmail.com>
2023-06-17 09:07:21 +0200

    16- essai d'animations sur page intro

--- src/components/Skills/Experiences.jsx
+++ src/components/Skills/Experiences.jsx
@@ -8,23 +8,29 @@ const Experiences = () => {
         <h4>Etudes développeur web chez OPENCLASSROOMS</h4>
         <h5>sept-2022 à mai-2023</h5>
         <p>
-          Apprentissages par projets de HTML5, CSS3, JAVASCRIPT, REACT etc...
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet unde
-          quam aspernatur nam, id amet suscipit eum numquam sed corrupti
-          repudiandae soluta natus quis possimus reprehenderit ab modi dolorum
-          itaque pariatur repellendus.
+          Apprentissages par projets de HTML5, CSS3, JAVASCRIPT, REACT etc... -
+          Intégration et et mise en place de la partie Front End d'un site. -
+          Utilisation de Reacts et de ses composants pour des projets plus
+          modulables. - Création et mise en place de petites animations et
+          transitions en CSS. - Utilisation de CSS et SASS. - Mise en place
+          dynamique d'un visuel de site e-commerce avec gestion
+          panier/produits/API. - Création gestion et sécurisation d'une API. -
+          Optimisation de SEO de référencement et de performances avec audit et
+          rapport.
         </p>
       </div>
       <div className="exp-2">
-        <h4>Etudes développeur web chez OPENCLASSROOMS</h4>
-        <h5>sept-2022 à mai-2023</h5>
+        <h4>Recherche Alternance PHP ou React/JS</h4>
+        <h5>avril-202 à maintenant</h5>
         <p>
-          Apprentissages par projets de HTML5, CSS3, JAVASCRIPT, REACT etc...
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          obcaecati deserunt tempore veniam at est commodi aspernatur explicabo
-          doloremque magni, impedit fuga porro aperiam ullam quod ipsa quo
-          veritatis, facere optio minima accusantium consequatur assumenda
-          corrupti magnam.
+          Je continue mon apprentissage en totale autonomie jusqu'à trouver un
+          contrat de professionalisation. En attendant je travail sur des
+          projets plus personnels comme mon Portfolio Interactif, une appli
+          météo web et mobile ou bien encore la refonte de mon propre site
+          internet qui date d'une dizaine d'années et que j'avais créé
+          rapidement sur wordpress. Tout cela me permet de continuer le code
+          tout en apprenant de nouvelles fonctionnalités ou de nouveaux langages
+          et de me perfectionner sur ce que j'ai déjà appris.
         </p>
       </div>
     </div>
--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -39,13 +39,17 @@ export default function Intro() {
 
   return (
     <div className="bodyIntro">
+      <div className="congrats">
+        <h1>FÉLICITATIONS!</h1>
+        <h2>vous venez de découvrir une carte "Rare"</h2>
+        <h3>Cliquez sur la carte pour en savoir plus!</h3>
+      </div>
       <Link to="/home">
         <div className="cardContainer">
           <div className="card" ref={cardRef}>
-            <div className="cardContent front">
+            <div className="cardContent">
               <img src="./media/moi1.jpg" alt="ma tête de bg!" />
             </div>
-            <div className="cardContent back"></div>
             <div className="glare"></div>
           </div>
         </div>
