346b0e1fae4f70e560fc298438bd3d690a01182b
YoannPitance <pittyo88@gmail.com>
2023-06-22 15:06:47 +0200

    21- logique d'affiche de nom de réseau au survol (navigation)

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
+  const [socialName, setSocialName] = useState("");
+  const handleHover = (className) => {
+    setSocialName(className);
+  };
+
   return (
     <div className="sidebar">
       <div className="id">
@@ -65,40 +70,56 @@ const Navigation = () => {
 
       <div className="socialNetwork">
         <ul>
-          <li>
+          <li
+            className={socialName === "Linkedin" ? "navActive" : ""}
+            onMouseEnter={() => handleHover("Linkedin")}
+            onMouseLeave={() => handleHover("")}
+          >
             <a
               href="https://www.linkedin.com/in/yoann-pitance-8a974324b"
-              className="linkedin"
+              className="Linkedin"
               rel="noreferrer"
               target="_blank"
             >
               <i className="fa-brands fa-linkedin" alt="Linkedin"></i>
             </a>
           </li>
-          <li>
+          <li
+            className={socialName === "Github" ? "navActive" : ""}
+            onMouseEnter={() => handleHover("Github")}
+            onMouseLeave={() => handleHover("")}
+          >
             <a
               href="https://github.com/YoannPitance"
-              className="github"
+              className="Github"
               rel="noreferrer"
               target="_blank"
             >
               <i className="fa-brands fa-github" alt="github"></i>
             </a>
           </li>
-          <li>
+          <li
+            className={socialName === "Instagram" ? "navActive" : ""}
+            onMouseEnter={() => handleHover("Instagram")}
+            onMouseLeave={() => handleHover("")}
+          >
             <a
               href="https://www.instagram.com/_pittyart_/"
-              className="instagram"
+              className="Instagram"
               rel="noreferrer"
               target="_blank"
             >
               <i className="fa-brands fa-instagram" alt="instagram"></i>
             </a>
           </li>
-          <li>
+          <li
+            className={socialName === "Facebook" ? "navActive" : ""}
+            onMouseEnter={() => handleHover("Facebook")}
+            onMouseLeave={() => handleHover("")}
+          >
             <a
               href="https://www.facebook.com/Pittyart88/"
-              className="facebook"
+              className="Facebook"
               rel="noreferrer"
               target="_blank"
             >
@@ -106,8 +127,9 @@ const Navigation = () => {
             </a>
           </li>
         </ul>
+        <div className="socialName">{socialName && <p>{socialName}</p>}</div>
         <div className="signature">
-          <p>yoannPitance - 2023 -</p>
+          <p>yoannPitance'Dev - 2023 -</p>
         </div>
       </div>
     </div>
